b82a01c054965fb2cc89732e934cc5ed4aed9257
junha <goodjunha@gmail.com>
2023-06-16 11:47:12 +0900

    snpkit:: safeAreaLayoutGuide를 사용하지 않아서 이탈되는 코드 예시

---
+++ SnpkitLayout/SnpkitLayout/example/ExampleUIViewController7.swift
@@ -0,0 +1,41 @@
+//
+//  ExampleUIViewController7.swift
+//  SnpkitLayout
+//
+//  Created by 박준하 on 2023/06/16.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+// 위에 네비게이션을 이탈하고 주황색으로 채우는 코드
+class ExampleUIViewCotroller7: UIViewController {
+    
+    lazy var orangeView = UIView(frame: .zero)
+    let boxWidth: CGFloat = 100
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        orangeView.backgroundColor = .orange
+        
+        layout()
+    }
+
+    func layout() {
+        [
+            orangeView
+        ].forEach { view.addSubview($0) }
+        
+        orangeView.snp.makeConstraints {
+            $0.height.equalTo(boxWidth)
+            $0.leading.trailing.equalToSuperview()
+            $0.top.equalToSuperview()
+        }
+    }
+    
+    /*
+     위에 네비게이션 부분을 채웁니다.
+     */
+}
