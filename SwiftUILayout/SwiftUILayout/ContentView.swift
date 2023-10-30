2f0785defbef27d83640a042c7db7b867709c0f3
junha <goodjunha@gmail.com>
2023-10-30 15:18:41 +0900

    SwiftUI:: 프로젝트 세팅

---
+++ SwiftUILayout/SwiftUILayout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILayout
+//
+//  Created by 박준하 on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILayout/SwiftUILayout/SwiftUILayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILayoutApp.swift
+//  SwiftUILayout
+//
+//  Created by 박준하 on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
